e01e11c11bc51584b92e75413fc37073f54a7dd9
3ndrius <andrzejo1991@gmail.com>
2019-04-01 16:52:58 +0200

    Adding user schema db

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongooge = require('mongoose');
+
+const Schema = mongooge.Schema;
+
+const userSchema = new Schema({
+
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    createdEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Event'
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema);
