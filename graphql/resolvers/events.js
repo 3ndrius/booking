e33790e9476a24602002f1a5f99f82639167ccc1
3ndrius <andrzejo1991@gmail.com>
2019-04-04 21:17:35 +0200

    Bug user not found

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -29,13 +29,13 @@ module.exports = {
         description: args.eventInput.description,
         price: +args.eventInput.price,
         date: dateToString(args.eventInput.date),
-        creator: '5ca65532b6b1061d68e5c3db'
+        creator:'5ca6571dc548aa3734478290'
       });
       let createdEvent;
       try {
         const result = await event.save();
         createdEvent = transformEvent(result);
-        const creator = await User.findById('5ca65532b6b1061d68e5c3db');
+        const creator = await User.findById('5ca6571dc548aa3734478290');
   
         if (!creator) {
           throw new Error('User not found.');
