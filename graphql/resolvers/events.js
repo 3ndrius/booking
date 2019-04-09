a943ed361ebd0e78db0091a8254043f02680269e
3ndrius <andrzejo1991@gmail.com>
2019-04-09 11:07:02 +0200

    Re-copy from my local

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,56 +1,46 @@
-const { dateToString } = require('../../helpers/date');
 const Event = require('../../models/event');
+const User = require('../../models/user');
 
-
-
-
-const transformEvent = event => {
-    return {
-      ...event._doc,
-      _id: event.id,
-      date: dateToString(event._doc.date),
-      creator: user.bind(this, event.creator)
-    }
-  }
+const { transformEvent } = require('./merge');
 
 module.exports = {
-    events: async () => {
-      try {
-        const events = await Event.find();
-        return events.map(event => {
-          return transformEvent(event);
-        });
-      } catch (err) {
-        throw err;
-      }
-    },
-    createEvent: async args => {
-      const event = new Event({
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: dateToString(args.eventInput.date),
-        creator:'5ca65532b6b1061d68e5c3db'
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
       });
-      let createdEvent;
-      try {
-        const result = await event.save();
-        createdEvent = transformEvent(result);
-        const creator = await User.findById('5ca65532b6b1061d68e5c3db');
-  
-        if (!creator) {
-          throw new Error('User not found.');
-        }
-        creator.createdEvents.push(event);
-        await creator.save();
-  
-        return createdEvent;
-      } catch (err) {
-        console.log(err);
-        throw err;
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated!');
+    }
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: req.userId
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById(req.userId);
+
+      if (!creator) {
+        throw new Error('User not found.');
       }
-    },
-    
-  
-    
-  };
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
