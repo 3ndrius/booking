adc09b5125706e6f5d8a345c510d5e4d91df10b4
3ndrius <andrzejo1991@gmail.com>
2019-04-11 09:53:07 +0200

    Adding react-router to front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-   
-      </div>
+      <BrowserRouter>
+       <Switch>
+         <Route path="/" exact component={null} />
+         <Route path="/auth" exact component={null} />
+         <Route path="/events" exact component={null} />
+         <Route path="/bookings" exact component={null} />
+       </Switch>
+      </BrowserRouter>
     );
   }
 }
