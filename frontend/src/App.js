9cfc5c5d4a50df8453edb0a5c1cc91617027581a
3ndrius <andrzejo1991@gmail.com>
2019-04-15 10:37:33 +0200

    Adding context provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,22 +7,24 @@ import Bookings from './pages/Bookings';
 import Events from './pages/Events';
 import MainNavigation from './components/Navigation/MainNavigation';
 
-
+import AuthContext from './context/auth-context';
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-     <MainNavigation/>
-     <main className="main-content">
-     <Switch>
-         <Redirect path="/" to="/auth" exact />
-         <Route path="/auth" component={Auth} />
-         <Route path="/events"  component={Events} />
-         <Route path="/bookings" component={Bookings} />
-       </Switch>
-     </main>
+      <AuthContext.Provider>
+        <MainNavigation/>
+        <main className="main-content">
+        <Switch>
+            <Redirect path="/" to="/auth" exact />
+            <Route path="/auth" component={Auth} />
+            <Route path="/events"  component={Events} />
+            <Route path="/bookings" component={Bookings} />
+          </Switch>
+        </main>
+     </AuthContext.Provider>
       </BrowserRouter>
     );
   }
