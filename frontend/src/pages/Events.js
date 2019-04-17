f0156cf31fd713383a22b4c651ccf3ad724a2051
3ndrius <andrzejo1991@gmail.com>
2019-04-17 21:01:55 +0200

    Finish modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,15 +3,35 @@ import './Events.css';
 
 import Modal from './../components/Modal/Modal';
 
+import Backdrop from './../components/Backdrop/Backdrop';
 
 export default class Events extends Component {
+
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ 
+      creating: true
+    });
+  }
+  modalConfirmHandler = () =>{
+
+  }
+  modalCancelHandler = () => {
+    this.setState({
+      creating:false
+    })
+  }
   render() {
     return (
       <React.Fragment>
-        <Modal>Modal content</Modal>
+      {this.state.creating && <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler} >Modal content</Modal>}
+      {this.state.creating && <Backdrop/>}
       <div className="events">
         <h2>Share with us events</h2>
-        <button onClick={this.handleCreate}>Create event</button>
+        <button onClick={this.startCreateEventHandler}>Create event</button>
       </div>
       </React.Fragment>
     )
