52d6e2d7ebb6790783c12b43d1bfea8682c17504
3ndrius <andrzejo1991@gmail.com>
2019-04-29 12:25:12 +0200

    Adding event info

--- frontend/src/components/Events/EventList/EventItem/EventItem.js
+++ frontend/src/components/Events/EventList/EventItem/EventItem.js
@@ -6,7 +6,14 @@ export default function EventItem(props) {
 
         return (
           <li key={props.eventId} className="events__list-item">
-            {props.title}
+            <div className="eventItem__header"> 
+                <h1>{props.title}</h1>
+                <h2>$10.99</h2>
+            </div>
+            <div>
+                <button className="btn">View details</button>
+                <p>You are creator of this event</p>
+            </div>
           </li>
         );
  
