090be6fa42f73f599ae04085ecd5e4c7017ac572
plcodepc <codepc@interia.pl>
2019-05-09 18:49:51 +0200

    Adding fetch booking

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,55 @@
 import React, { Component } from 'react'
 
 export default class Bookings extends Component {
+
+  state = {
+    isLoading: false,
+    bookings: []
+  }
+  componentDidMount = () =>{
+    this.fetchBookings();
+  }
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          query {
+            bookings {
+              _id
+             createdAt
+             event {
+               _id
+               title
+               date
+             }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings: bookings, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  }
   render() {
     return (
       <div>
