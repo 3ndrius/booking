ea9b845a3976c0e611807b7667df7889e0c33eaf
3ndrius <andrzejo1991@gmail.com>
2019-05-16 12:00:42 +0200

    Adding chart data

--- frontend/src/components/Bookings/BookingsChart/BookingsChart.js
+++ frontend/src/components/Bookings/BookingsChart/BookingsChart.js
@@ -1,30 +1,54 @@
-import React from 'react'
+import React from 'react';
 
 
 const BOOKINGS_BUCKETS = {
-    'Cheap': 100,
-    'Normal': 200,
-    'Expensive': 10000
+  Cheap: {
+    min: 0,
+    max: 100
+  },
+  Normal: {
+    min: 100,
+    max: 200
+  },
+  Expensive: {
+    min: 200,
+    max: 10000000
+  }
 };
 
-export default function BookingsChart(props) {
-
-  let output = {};
-  for(const bucket in BOOKINGS_BUCKETS) {
-     const filteredBookings = props.bookings.reduce((prev, current ) => {
-       if(current.price < BOOKINGS_BUCKETS[bucket]) {
+const bookingsChart = props => {
+  const chartData = { labels: [], datasets: [] };
+  let values = [];
+  for (const bucket in BOOKINGS_BUCKETS) {
+    const filteredBookingsCount = props.bookings.reduce((prev, current) => {
+      if (
+        current.event.price > BOOKINGS_BUCKETS[bucket].min &&
+        current.event.price < BOOKINGS_BUCKETS[bucket].max
+      ) {
         return prev + 1;
-       }
-       else{
-         return prev;
-       }
-      
-      }, 0);
-      output[bucket] = filteredBookingsCount
+      } else {
+        return prev;
+      }
+    }, 0);
+    values.push(filteredBookingsCount);
+    chartData.labels.push(bucket);
+    chartData.datasets.push({
+      // label: "My First dataset",
+      fillColor: 'rgba(220,220,220,0.5)',
+      strokeColor: 'rgba(220,220,220,0.8)',
+      highlightFill: 'rgba(220,220,220,0.75)',
+      highlightStroke: 'rgba(220,220,220,1)',
+      data: values
+    });
+    values = [...values];
+    values[values.length - 1] = 0;
   }
+
   return (
-    <div>
-      chart
+    <div style={{ textAlign: 'center' }}>
+      {/* <BarChart data={chartData} /> */}
     </div>
-  )
-}
+  );
+};
+
+export default bookingsChart;
