560225197093f09644b59aca2cafef10a199fdab
3ndrius <andrzejo1991@gmail.com>
2019-05-16 11:59:44 +0200

    Adding bookings chart component

--- frontend/src/components/Bookings/BookingsChart/BookingsChart.js
+++ frontend/src/components/Bookings/BookingsChart/BookingsChart.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
+
+const BOOKINGS_BUCKETS = {
+    'Cheap': 100,
+    'Normal': 200,
+    'Expensive': 10000
+};
+
 export default function BookingsChart(props) {
+
+  let output = {};
+  for(const bucket in BOOKINGS_BUCKETS) {
+     const filteredBookings = props.bookings.reduce((prev, current ) => {
+       if(current.price < BOOKINGS_BUCKETS[bucket]) {
+        return prev + 1;
+       }
+       else{
+         return prev;
+       }
+      
+      }, 0);
+      output[bucket] = filteredBookingsCount
+  }
   return (
     <div>
       chart
